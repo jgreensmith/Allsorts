c3f464f687103491b648fc1ddd0ddc6ca6c5ba46
James <jgreensmith13@gmail.com>
2025-07-31 20:40:15 +0100

    asdfasfd

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,17 +1,27 @@
+//               <Link to='/parent'>Parent Documents</Link> 
+//             </MenuItem>
+//             <MenuItem>
+//               <Link to='/eyfs'>EYFS</Link> 
+//             </MenuItem>
+//             <MenuItem>
+//               <Link to='/policies'>Policies and Procedures</Link> 
+//             </MenuItem>
+//           </MenuList>
+//         </Menu>
+      
+      
+//     );
+//   }
+
+//   export default DropDown;
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import MenuItems from '../menuitems';
-
 import './dropdown.scss';
 
 function Dropdown() {
-
     const [click, setClick] = useState(false);
-
     const handleClick = () => setClick(!click);
-
-
-
     return(
         <>
         <ul 
@@ -33,61 +43,7 @@ function Dropdown() {
             })}
         </ul>
         </>
-        
     );
-
 }
 
 export default Dropdown;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import {
-//     Menu,
-//     MenuList,
-//     MenuButton,
-//     MenuItem,
-//   } from "@reach/menu-button";
-//   import "@reach/menu-button/styles.css";
-//   import './dropdown.scss';
-//   import '../navbar/navbar.scss';
-   
-//   const DropDown = ({links}) => {
-//     return (
-//         <Menu>
-//           <MenuButton className="navbar-documents">
-//             Documents <span aria-hidden>▾</span>
-//           </MenuButton>
-//           <MenuList>
-//             <MenuItem>
-//               <Link to='/parent'>Parent Documents</Link> 
-//             </MenuItem>
-//             <MenuItem>
-//               <Link to='/eyfs'>EYFS</Link> 
-//             </MenuItem>
-//             <MenuItem>
-//               <Link to='/policies'>Policies and Procedures</Link> 
-//             </MenuItem>
-//           </MenuList>
-//         </Menu>
-      
-      
-//     );
-//   }
-
-//   export default DropDown;
